87d4dd228e11dc4536785d426870ed695bb76a1f
Kenji0510 <yukkurireimupikacyuu@gmail.com>
2025-07-04 14:27:39 +0900

    20250704

--- src/main.rs
+++ src/main.rs
@@ -40,7 +40,19 @@ fn main() {
 
     let pcd_data = match load_pcd::load_pcd(
         // "/Users/kenji/workspace/Rust/rerun-sample/data/Laser_map/Laser_map_35.pcd",
-        "/Users/kenji/Downloads/combined_120.pcd",
+        "/Users/kenji/Downloads/combined_220.pcd",
+        // "/home/kenji/workspace/Rust/voxelization-pcd-rust/data/combined_120.pcd",
+    ) {
+        Ok(points) => points,
+        Err(e) => {
+            eprintln!("Error loading PCD file: {}", e);
+            return;
+        }
+    };
+
+    let pcd_data_target = match load_pcd::load_pcd(
+        // "/Users/kenji/workspace/Rust/rerun-sample/data/Laser_map/Laser_map_35.pcd",
+        "/Users/kenji/Downloads/combined_100.pcd",
         // "/home/kenji/workspace/Rust/voxelization-pcd-rust/data/combined_120.pcd",
     ) {
         Ok(points) => points,
@@ -51,12 +63,13 @@ fn main() {
     };
 
     let pcd_points = convert_to_f32(&pcd_data);
+    let pcd_target_points = convert_to_f32(&pcd_data_target);
     if pcd_points.is_empty() {
         println!("No arguments provided. Please provide a list of floats.");
         return;
     }
 
-    let voxel_size = 0.05;
+    let voxel_size = 0.1;
     let (min_x, min_y, min_z) = get_min_value(&pcd_data);
     println!("Original points: {}", pcd_points.len() / 3);
 
@@ -83,6 +96,7 @@ fn main() {
         "max_storage_buffer_binding_size: {:?}",
         limits.max_storage_buffers_per_shader_stage
     );
+    println!("max_buffer_size: {:?}", limits.max_buffer_size);
 
     let (device, queue) = pollster::block_on(adapter.request_device(&wgpu::DeviceDescriptor {
         label: None,
@@ -107,27 +121,10 @@ fn main() {
         hash_mask: (capacity - 1) as u32,
         _pad: 0.0,
     };
-    let zero_i32 = vec![0i32; capacity as usize];
 
     let table_keys = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: Some("table_keys"),
         contents: bytemuck::cast_slice(&zero_key),
-        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
-    });
-
-    let sum_x_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: Some("sum_x"),
-        contents: bytemuck::cast_slice(&zero_i32),
-        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
-    });
-    let sum_y_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: Some("sum_y"),
-        contents: bytemuck::cast_slice(&zero_i32),
-        usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
-    });
-    let sum_z_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
-        label: Some("sum_z"),
-        contents: bytemuck::cast_slice(&zero_i32),
         usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
     });
 
@@ -149,13 +146,13 @@ fn main() {
         usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
     });
 
-    let centroids_num = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+    let out_cnt = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: Some("centroids_num"),
         contents: bytemuck::bytes_of(&0u32),
         usage: wgpu::BufferUsages::STORAGE | wgpu::BufferUsages::COPY_SRC,
     });
 
-    let points_data_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+    let input_points_data_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
         label: None,
         contents: bytemuck::cast_slice(&pcd_points),
         usage: wgpu::BufferUsages::STORAGE
@@ -163,42 +160,60 @@ fn main() {
             | wgpu::BufferUsages::COPY_DST,
     });
 
+    let input_target_points_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: None,
+        contents: bytemuck::cast_slice(&pcd_target_points),
+        usage: wgpu::BufferUsages::STORAGE
+            | wgpu::BufferUsages::COPY_SRC
+            | wgpu::BufferUsages::COPY_DST,
+    });
+
+    let output_points_data_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: None,
+        size: (pcd_points.len() * std::mem::size_of::<f32>()) as u64,
+        usage: wgpu::BufferUsages::STORAGE
+            | wgpu::BufferUsages::COPY_SRC
+            | wgpu::BufferUsages::COPY_DST,
+        mapped_at_creation: false,
+    });
+
     let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
         label: None,
         entries: &[
+            // 0: input_points_data_buffer
             wgpu::BindGroupLayoutEntry {
                 binding: 0,
                 visibility: wgpu::ShaderStages::COMPUTE,
                 ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: false },
+                    ty: wgpu::BufferBindingType::Storage { read_only: true },
                     min_binding_size: None,
                     has_dynamic_offset: false,
                 },
                 count: None,
             },
-            // 1: table_keys  (read-write)
+            // 1: input_target_points_buffer
             wgpu::BindGroupLayoutEntry {
                 binding: 1,
                 visibility: wgpu::ShaderStages::COMPUTE,
                 ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: false },
-                    has_dynamic_offset: false,
+                    ty: wgpu::BufferBindingType::Storage { read_only: true },
                     min_binding_size: None,
+                    has_dynamic_offset: false,
                 },
                 count: None,
             },
-            // 2: sum_x
+            // 2: output_points_data_buffer
             wgpu::BindGroupLayoutEntry {
                 binding: 2,
                 visibility: wgpu::ShaderStages::COMPUTE,
                 ty: wgpu::BindingType::Buffer {
                     ty: wgpu::BufferBindingType::Storage { read_only: false },
-                    has_dynamic_offset: false,
                     min_binding_size: None,
+                    has_dynamic_offset: false,
                 },
                 count: None,
             },
-            // 3: sum_y
+            // 3: table_keys  (read-write)
             wgpu::BindGroupLayoutEntry {
                 binding: 3,
                 visibility: wgpu::ShaderStages::COMPUTE,
@@ -209,7 +224,7 @@ fn main() {
                 },
                 count: None,
             },
-            // 4: sum_z
+            // 4: table_cnt
             wgpu::BindGroupLayoutEntry {
                 binding: 4,
                 visibility: wgpu::ShaderStages::COMPUTE,
@@ -220,7 +235,7 @@ fn main() {
                 },
                 count: None,
             },
-            // 5: table_cnt
+            // 5: fail_counter
             wgpu::BindGroupLayoutEntry {
                 binding: 5,
                 visibility: wgpu::ShaderStages::COMPUTE,
@@ -231,20 +246,9 @@ fn main() {
                 },
                 count: None,
             },
-            // 6: fail_counter
+            // 6: Uniforms
             wgpu::BindGroupLayoutEntry {
                 binding: 6,
-                visibility: wgpu::ShaderStages::COMPUTE,
-                ty: wgpu::BindingType::Buffer {
-                    ty: wgpu::BufferBindingType::Storage { read_only: false },
-                    has_dynamic_offset: false,
-                    min_binding_size: None,
-                },
-                count: None,
-            },
-            // 7: Uniforms
-            wgpu::BindGroupLayoutEntry {
-                binding: 7,
                 visibility: wgpu::ShaderStages::COMPUTE,
                 ty: wgpu::BindingType::Buffer {
                     ty: wgpu::BufferBindingType::Uniform,
@@ -253,9 +257,9 @@ fn main() {
                 },
                 count: None,
             },
-            // 8. centroids_num
+            // 7. out_cnt
             wgpu::BindGroupLayoutEntry {
-                binding: 8,
+                binding: 7,
                 visibility: wgpu::ShaderStages::COMPUTE,
                 ty: wgpu::BindingType::Buffer {
                     ty: wgpu::BufferBindingType::Storage { read_only: false },
@@ -273,39 +277,35 @@ fn main() {
         entries: &[
             wgpu::BindGroupEntry {
                 binding: 0,
-                resource: points_data_buffer.as_entire_binding(),
+                resource: input_points_data_buffer.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
                 binding: 1,
-                resource: table_keys.as_entire_binding(),
+                resource: input_target_points_buffer.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
                 binding: 2,
-                resource: sum_x_buf.as_entire_binding(),
+                resource: output_points_data_buffer.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
                 binding: 3,
-                resource: sum_y_buf.as_entire_binding(),
+                resource: table_keys.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
                 binding: 4,
-                resource: sum_z_buf.as_entire_binding(),
-            },
-            wgpu::BindGroupEntry {
-                binding: 5,
                 resource: table_cnt.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
-                binding: 6,
+                binding: 5,
                 resource: fail_counter.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
-                binding: 7,
+                binding: 6,
                 resource: uniform_buf.as_entire_binding(),
             },
             wgpu::BindGroupEntry {
-                binding: 8,
-                resource: centroids_num.as_entire_binding(),
+                binding: 7,
+                resource: out_cnt.as_entire_binding(),
             },
         ],
     });
@@ -329,7 +329,7 @@ fn main() {
         label: None,
         layout: Some(&pipeline_layout),
         module: &module,
-        entry_point: Some("centroid_main"),
+        entry_point: Some("bg_subtraction_main"),
         compilation_options: wgpu::PipelineCompilationOptions::default(),
         cache: None,
     });
@@ -378,8 +378,8 @@ fn main() {
         mapped_at_creation: false,
     });
 
-    let staging_centroids_num = device.create_buffer(&wgpu::BufferDescriptor {
-        label: Some("staging_centroids_num"),
+    let staging_out_cnt = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("staging_out_cnt"),
         size: std::mem::size_of::<u32>() as u64,
         usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::MAP_READ,
         mapped_at_creation: false,
@@ -395,9 +395,9 @@ fn main() {
     );
 
     encoder.copy_buffer_to_buffer(
-        &centroids_num, // src
+        &out_cnt, // src
         0,
-        &staging_centroids_num, // dst
+        &staging_out_cnt, // dst
         0,
         std::mem::size_of::<u32>() as u64,
     );
@@ -411,15 +411,15 @@ fn main() {
             mapped_at_creation: false,
         })
     }
-    let d_buffer = staging(&device, "d_buffer", points_data_buffer.size());
+    let d_buffer = staging(&device, "d_buffer", input_points_data_buffer.size());
 
     // 2. コピー命令をエンコード（encoder は新しく作り直す）
     encoder.copy_buffer_to_buffer(
-        &points_data_buffer,
+        &input_points_data_buffer,
         0,
         &d_buffer,
         0,
-        points_data_buffer.size(),
+        input_points_data_buffer.size(),
     );
 
     device.poll(wgpu::PollType::Wait).unwrap();
@@ -448,14 +448,14 @@ fn main() {
     drop(data);
     staging_fail.unmap();
 
-    let cent_slice = staging_centroids_num.slice(..);
-    cent_slice.map_async(wgpu::MapMode::Read, |_| ());
+    let out_cnt_slice = staging_out_cnt.slice(..);
+    out_cnt_slice.map_async(wgpu::MapMode::Read, |_| ());
     device.poll(wgpu::PollType::Wait).unwrap(); // ブロッキング待機
 
-    let cent_data = cent_slice.get_mapped_range();
-    let cent_num_value = *bytemuck::from_bytes::<u32>(&cent_data);
-    drop(cent_data);
-    staging_centroids_num.unmap();
+    let out_cnt_data = out_cnt_slice.get_mapped_range();
+    let out_cnt_value = *bytemuck::from_bytes::<u32>(&out_cnt_data);
+    drop(out_cnt_data);
+    staging_out_cnt.unmap();
 
     assert!(
         fail_val == 0,
@@ -464,7 +464,7 @@ fn main() {
     );
 
     println!("Compute shader finished — fail_cnt = {}", fail_val);
-    println!("Number of Centroids: {}", &cent_num_value);
+    println!("Number of output points: {}", &out_cnt_value);
 
     // 3. マップ & スライス
     macro_rules! map_slice {
@@ -478,32 +478,31 @@ fn main() {
 
     let m_d_buffer = map_slice!(d_buffer);
 
-    let centroids_ref: &[f32] = bytemuck::cast_slice(&m_d_buffer);
+    let output_points_ref: &[f32] = bytemuck::cast_slice(&m_d_buffer);
 
-    let mut centroids = Vec::new();
+    let mut output_points = Vec::new();
 
-    for i in 0..cent_num_value as usize {
-        centroids.push(Point {
-            x: centroids_ref[i * 3],
-            y: centroids_ref[i * 3 + 1],
-            z: centroids_ref[i * 3 + 2],
+    for i in 0..out_cnt_value as usize {
+        output_points.push(Point {
+            x: output_points_ref[i * 3],
+            y: output_points_ref[i * 3 + 1],
+            z: output_points_ref[i * 3 + 2],
         })
     }
 
     let elpased = start.elapsed();
-    // 4. 重心計算
     drop(m_d_buffer);
 
     // // アンマップ
     d_buffer.unmap();
 
     println!("Processed time: {:?}", elpased);
-    println!("Voxelized points: {}", centroids.len());
+    println!("After back subtraction points: {}", output_points.len());
 
-    match load_pcd::save_pcd("aa", centroids) {
+    match load_pcd::save_pcd("aa", output_points) {
         Ok(()) => println!("Successfully to save!"),
         Err(_) => {
-            eprintln!("Failed to save voxelized pcd!");
+            eprintln!("Failed to save the back subtracted pcd!");
             return;
         }
     };
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -7,19 +7,14 @@ struct Uniforms {
     hash_mask: u32, // capasity
 };
 
-@group(0) @binding(0) var<storage, read_write>  points : array<Point>;
-@group(0) @binding(1) var<storage, read_write> table_keys : array<atomic<u32>>;
-@group(0) @binding(2) var<storage, read_write> sum_x : array<atomic<i32>>;
-@group(0) @binding(3) var<storage, read_write> sum_y : array<atomic<i32>>;
-@group(0) @binding(4) var<storage, read_write> sum_z : array<atomic<i32>>;
-@group(0) @binding(5) var<storage, read_write> table_cnt  : array<atomic<u32>>;
-@group(0) @binding(6) var<storage, read_write> fail_cnt   : atomic<u32>;
-@group(0) @binding(7) var<uniform>             uni        : Uniforms;
-@group(0) @binding(8) var<storage, read_write> centroids_num : atomic<u32>;
-
-var<workgroup> w_key  : array<u32, 256>;
-var<workgroup> w_sum  : array<vec3<f32>, 256>;
-var<workgroup> w_cnt  : array<u32, 256>;
+@group(0) @binding(0) var<storage, read>  in_back_pts : array<Point>;
+@group(0) @binding(1) var<storage, read>  in_target_pts : array<Point>;
+@group(0) @binding(2) var<storage, read_write>  out_pts : array<Point>;
+@group(0) @binding(3) var<storage, read_write> bg_table_keys : array<atomic<u32>>;
+@group(0) @binding(4) var<storage, read_write> table_cnt  : array<atomic<u32>>;
+@group(0) @binding(5) var<storage, read_write> fail_cnt   : atomic<u32>;
+@group(0) @binding(6) var<uniform>             uni        : Uniforms;
+@group(0) @binding(7) var<storage, read_write> out_pts_num : atomic<u32>;
 
 fn part1by2(n: u32) -> u32 {
     // “n” のビットを 0b00n00n00n… の形に広げる関数
@@ -39,72 +34,47 @@ fn main(@builtin(global_invocation_id) gid: vec3<u32>,
         @builtin(local_invocation_id)  lid: vec3<u32>) {
 
   let idx = gid.x;
-  if (idx >= arrayLength(&points)) { return; }
+  if (idx >= arrayLength(&in_back_pts)) { return; }
 
   // --- KeyGen --------------------------------
-  let p = points[idx];
+  let p = in_back_pts[idx];
   let p_vec = vec3<f32>(p.x, p.y, p.z);
   let v = (p_vec - uni.origin) * uni.inv_vox;
   let key = morton3d(u32(floor(v.x)),u32(floor(v.y)),u32(floor(v.z)));
 
-  w_key[lid.x] = key;
-  w_sum[lid.x] = vec3<f32>(p.x, p.y, p.z);
-  w_cnt[lid.x] = 1u;
-  workgroupBarrier();
-
-  // --- WG 内 Reduction (隣り合う同キーをまとめる) ----
-  var step:u32 = 128u;
-  loop {
-      if (lid.x < step && w_key[lid.x] == w_key[lid.x + step]) {
-          w_sum[lid.x] += w_sum[lid.x + step];
-          w_cnt[lid.x] += w_cnt[lid.x + step];
-          w_key[lid.x + step] = 0xffffffffu;   // 無効化
-      }
-      workgroupBarrier();
-      if (step == 1u) { break; }
-      step = step >> 1u;
-  }
-
-  // --- Global Hash Insert ---------------------
-  if (w_key[lid.x] != 0xffffffffu) {
-        var slot = w_key[lid.x] & uni.hash_mask;
-        for (var tries:u32=0u; tries<64u; tries=tries+1u) {
-            let prev = atomicCompareExchangeWeak(&table_keys[slot], 0u, w_key[lid.x]);
-            if (prev.exchanged || prev.old_value == w_key[lid.x]) {
-                // 座標を整数スケールへ → i32
-                let sx = i32(round(w_sum[lid.x].x * uni.scale));
-                let sy = i32(round(w_sum[lid.x].y * uni.scale));
-                let sz = i32(round(w_sum[lid.x].z * uni.scale));
-
-                atomicAdd(&sum_x[slot], sx);
-                atomicAdd(&sum_y[slot], sy);
-                atomicAdd(&sum_z[slot], sz);
-                atomicAdd(&table_cnt[slot], w_cnt[lid.x]);  // u32
-                return;
-            }
-            slot = (slot + 1u) & uni.hash_mask;
-        }
-        // 挿入失敗
-        atomicAdd(&fail_cnt, 1u);
-  }
+  atomicStore(&bg_table_keys[key], 1u);
 }
 
 @compute @workgroup_size(256)
-fn centroid_main(@builtin(global_invocation_id) gid: vec3<u32>) {
+fn bg_subtraction_main(@builtin(global_invocation_id) gid: vec3<u32>) {
     let i = gid.x;
-    if (i >= uni.hash_mask) {
-        return;
+    // if (i >= uni.hash_mask) {
+    //     return;
+    // }
+
+    if (i >= arrayLength(&in_back_pts)) { return; }
+
+    let p = in_target_pts[i];
+    let p_vec = vec3<f32>(p.x, p.y, p.z);
+    let v = (p_vec - uni.origin) * uni.inv_vox;
+    let key = morton3d(u32(floor(v.x)),u32(floor(v.y)),u32(floor(v.z)));
+
+    if (atomicLoad(&bg_table_keys[key]) == 0u) {
+        let dst = atomicAdd(&out_pts_num, 1u);
+        out_pts[dst] = p;
     }
 
-    let k = atomicLoad(&table_keys[i]);
-    if (k == 0u) {
-        return;
-    }
-    let dst = atomicAdd(&centroids_num, 1u);
-    let c  = f32(atomicLoad(&table_cnt[i]));
-    let sx = f32(atomicLoad(&sum_x[i])) * uni.inv_scale;
-    let sy = f32(atomicLoad(&sum_y[i])) * uni.inv_scale;
-    let sz = f32(atomicLoad(&sum_z[i])) * uni.inv_scale;
+    return;
 
-    points[dst] = Point(sx / c, sy / c, sz / c);
+    // let k = atomicLoad(&bg_table_keys[i]);
+    // if (k == 0u) {
+    //     return;
+    // }
+    // let dst = atomicAdd(&out_cnt, 1u);
+    // let c  = f32(atomicLoad(&table_cnt[i]));
+    // let sx = f32(atomicLoad(&sum_x[i])) * uni.inv_scale;
+    // let sy = f32(atomicLoad(&sum_y[i])) * uni.inv_scale;
+    // let sz = f32(atomicLoad(&sum_z[i])) * uni.inv_scale;
+
+    // in_pts[dst] = Point(sx / c, sy / c, sz / c);
 }
